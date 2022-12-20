433ba99814e998441781b406492e77f1c97365c4
boltonlin <boltonlinbo@gmail.com>
2022-12-19 20:48:23 -0800

    passes all blinkyDancerSpec tests

--- src/dancer.js
+++ src/dancer.js
@@ -14,28 +14,23 @@
 
 // Creates and returns a new dancer object that can step
 var Dancer = function(top, left, timeBetweenSteps) {
-
-  // this.step();
-
-  // // now that we have defined the dancer object, we can start
-  // // setting up important parts of it by calling the methods we wrote
-  // // this one sets the position to some random default point within
-  // // the body
-  // this.setPosition(top, left);
+  // now that we have defined the dancer object, we can start
+  // setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within
+  // the body
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.top = top;
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
-  this.prototype.step.bind(this)();
-  this.prototype.setPosition.bind(this)(top, left);
-
+  this.step();
+  this.setPosition(top, left);
 };
 
 Dancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on
   // each step, it just schedules the next step
-  setTimeout(this.step, this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 };
 
 Dancer.prototype.setPosition = function(top, left) {
